5394b0013e18c8aafbe10761f5cb07426a99165d
Zaur Giyasov <zagiyas@mera.ru>
2018-07-30 11:45:48 +0300

    commit 1

--- PopoverExample/ViewController.m
+++ PopoverExample/ViewController.m
@@ -9,21 +9,53 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic) UIView *customView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self loading];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)loading {
+    [self createMainView];
+    [self createLocalNavibarButtons];
 }
 
+-(void)createMainView {
+    CGRect frame = [[UIScreen mainScreen] bounds];
+    frame.origin.y = 40;
+    self.customView = [[UIView alloc]initWithFrame:frame];
+    self.customView.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:self.customView];
+}
+
+-(void)createLocalNavibarButtons {
+    CGRect frame = CGRectMake(0, 0, self.view.frame.size.width, 50);
+    UINavigationBar *naviBar = [[UINavigationBar alloc] initWithFrame:frame];
+    
+    // Items
+    UINavigationItem *naviItem = [[UINavigationItem alloc] initWithTitle:@"example Title"];
+    UIBarButtonItem *cancelBarBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelButtonAction:)];
+    naviItem.leftBarButtonItem = cancelBarBtn;
+    
+    UIBarButtonItem *showBarItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(showButtonAction:)];
+    naviItem.rightBarButtonItem = showBarItem;
+    
+    [naviBar setItems:@[naviItem]];
+    [self.view addSubview:naviBar];
+}
+
+-(void)cancelButtonAction: (id)sender {
+    // cancel action
+}
+
+-(void)showButtonAction: (id)sender {
+    // show action
+}
 
 @end
